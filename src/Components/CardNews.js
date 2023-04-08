912529e72b47de5f6b2a83903a147187dad11018
Luis Fellype Botelho <79022593+luis-botelho@users.noreply.github.com>
2023-04-08 00:03:04 -0300

    Update CardNews.js

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -1,10 +1,27 @@
 class CardNews extends HTMLElement {
   constructor() {
     super();
-
     const shadow = this.attachShadow({ mode: "open" });
-    shadow.innerHTML = "<h1>Hello Word!</h1>";
+    shadow.appendChild(this.build());
+    shadow.appendChild(this.style());
   }
-}
 
+  //create the shadow component
+  build() {
+
+    const componentRoot = document.createElement("div");
+    componentRoot.setAttributes("class", "card");
+
+    const cardLeft = document.createElement("div");
+    cardLeft.setAttribute("class",  "card-left");
+
+    const cardRight = document.createElement("div");
+    cardRight.setAttribute("class", "card-right");
+
+    //assing children
+    componentRoot.appendChild(cardLeft);
+    componentRoot.appendChild(cardRight);
+  }
+  style() {}
+}
 customElements.define("card-news", CardNews);
